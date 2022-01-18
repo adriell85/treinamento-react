ac4714f712359cab175d925ef51395fbe41ddd19
adriell85 <gomes.adriel@gmail.com>
2022-01-18 15:02:47 -0300

    learning useContext + useReducer

---
+++ Project-five/src/App.js
@@ -0,0 +1,72 @@
+import P from 'prop-types';
+import { createContext, useReducer, useContext, useRef } from 'react';
+import './App.css';
+
+//actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+};
+
+// data.jsx
+export const globalState = {
+  title: 'O título do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+//reducer.js
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar Título');
+      return { ...state, title: action.payload };
+    }
+  }
+  return { ...state };
+};
+
+//AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
+  return (
+    <Context.Provider value={{ state, changeTitle }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+//H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+  return (
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}
+      </h1>
+      <input type="text" ref={inputRef}></input>
+    </>
+  );
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+function App() {
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
+  );
+}
+
+export default App;
